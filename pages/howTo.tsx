5d88c82c5205505a5b4fa9c230e6db21b1a796fd
Yoonsoo Kim <laptise@live.jp>
2021-05-02 14:10:25 +0900

    이용방법 샘플페이지

--- pages/howTo.tsx
+++ pages/howTo.tsx
@@ -1,7 +1,14 @@
 import React from "react";
+import Layout from "../components/Layout";
 
-/** 여기다가 만들어보기 */
+/**
+ * 이용방법페이지
+ */
 function HowTo() {
-  return <div>이용방법데스</div>;
+  return (
+    <Layout title="이용방법 | survey-next">
+      <h1>이용방법</h1>
+    </Layout>
+  );
 }
 export default HowTo;
